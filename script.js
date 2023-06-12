ebe8e8499d421477464fbe904afc20cf7db694ab
prince-anonymous <anonymoustech22555@gmail.com>
2023-06-13 00:18:40 +0100

    js practice

---
+++ script.js
@@ -0,0 +1,115 @@
+let js = 'amazing';
+if ('js ===amazing') alert('JavaScript is FUN!');
+2+50+5+10-2-5;
+console.log(2+50+5+10-2-5);
+var job = 'programmer'
+job = 'teacher'
+console.log(job)
+const firstName = 'orogiri'
+const lastName = 'ismail'
+console.log(firstName + ' '+ lastName)
+const now = 1996
+const ageSerah = now-2023
+const ageDekunle = now-2020
+console.log(ageDekunle, ageSerah)
+//ASSIGNMENT OPERATORS
+let x = 10 + 5 
+x += 10
+x *= 4
+x ++ ;
+x --;
+x --;
+console.log(x) // x = x + 10
+
+//comparison operators
+console.log(ageDekunle < ageSerah)
+console.log(ageDekunle >= ageSerah)
+
+
+let  v, y;
+v = y = 10-7-2
+console.log(v, y)
+//challenge 1
+/*const markHeight = 1.69
+const johnHeight = 1.95
+console.log(markHeight, johnHeight);
+const markMass = 78
+const johnMass =92
+console.log(markMass, johnMass);
+
+markBmi =(markMass / markHeight **2)
+console.log(markBmi)
+johnBmi = (johnMass/johnHeight **2)
+console.log(johnBmi)
+const higherBmi = markBmi > johnBmi
+console.log(higherBmi)*/
+/*
+const markHeight = 1.88
+const johnHeight = 1.76
+console.log(markHeight, johnHeight);
+const markMass = 95
+const johnMass = 85
+console.log(markMass, johnMass);
+
+markBmi =(markMass / markHeight **2)
+console.log(markBmi)
+johnBmi = (johnMass/johnHeight **2)
+console.log(johnBmi)
+const higherBmi = markBmi > johnBmi
+console.log(higherBmi)*/
+
+const aName = 'taiwo'
+const birthYear = 1995
+const year = 2023
+const profession = 'coder'
+const taiwo = "I'm "+ aName + ', a '+ (year-birthYear ) + ' years old ' + profession ;
+console.log(taiwo);
+//for template leatherals 
+const taiwoNew = `I'm ${aName}, a ${year-birthYear} years old ${profession}`
+console.log(taiwoNew);
+//string in a multiples line
+console.log(`string with \n\
+multiple \n\
+lines`);
+//if and else
+const age = 15;
+if (age>=20) {
+console.log('Damola can drive a car')}
+else {
+    const yearsLeft = 20-age;
+    console.log(`Damola is too young to drive a car wait another ${yearsLeft} years!.`)
+}
+const yearBorn= 2000
+let century;
+if(yearBorn<=2000 ) {
+     century = 20
+}
+else{
+    century=21
+}
+console.log(century)
+
+//challenge 2
+const markHeight = 1.88
+const johnHeight = 1.76
+console.log(markHeight, johnHeight);
+const markMass = 95
+const johnMass = 85
+console.log(markMass, johnMass);
+
+markBmi =(markMass / markHeight **2)
+console.log(markBmi)
+johnBmi = (johnMass/johnHeight **2)
+console.log(johnBmi)
+console.log(markBmi, johnBmi)
+const higherBmi = markBmi > johnBmi
+if("markBmi is higher than johnBmi"){
+    console.log("markBmi is higher than johnBmi")
+}
+else{
+    console.log(`${johnBmi} is higher than ${markBmi}`)
+}
+//type conversion nd coersion
+
+
+
